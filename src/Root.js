c59f08e9c200a6aeaca981177ff876c36810f5e8
이해빈 <luckylucy0717@gmail.com>
2019-05-27 23:06:22 +0900

    추가 파일

--- src/Root.js
+++ src/Root.js
@@ -1,4 +1,6 @@
 import React from 'react';
+// BrowserRotuer는 HTML5의 historyAPI 를 사용하여 새로고침하지 않고도 페이지를 교체할 수 있도록 해줌
+
 import {BrowserRouter} from 'react-router-dom';
 import App from './App';
 
--- src/pages/About.js
+++ src/pages/About.js
@@ -3,6 +3,7 @@ import queryString from 'query-string';
 
 const About = ({location, match}) => {
     const query = queryString.parse(location.search);
+    //받아온 모든 값이 문자열이다.
     const {color} = query;
     return (
         <div>
